6b2d962d1d84604b3b4582fee68858e35754de8f
qa-ltftb-3-dyle@example.com <qa-ltftb-3-dyle@example.com>
2023-05-15 13:51:54 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16464.stoplight-dev.com/api/v1/projects/cHJqOjQ3Ng/spectral.js?branch_slug=main&token=0267cdbe-5cb5-4264-8f3b-0efd4d666c6c";
+export default { extends: ruleset };
